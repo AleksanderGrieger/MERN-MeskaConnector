74b5c3f0b490ccc90a955a0b3e10eb8417f16de7
Aleksander Grieger <olek.g12.09.1999@interia.pl>
2021-02-28 21:08:37 +0100

    Section 5

--- routes/api/films.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// @route  GET api/films
-// @desc   Test route
-// @access Public
-router.get("/", (req, res) => res.send("Films route"));
-
-module.exports = router;
